9697f7f324c515996dd9c7b97fdf5263c0a4eddf
Karlo Longin <kl9523@rit.edu>
2023-12-04 23:16:23 +0100

    Implemented React and Firebase

---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore"; // Import Firestore
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBYKmfQhE1yITMYGlk8CEuqAwGyaZZFTWs",
+  authDomain: "master-scheduler-cbe64.firebaseapp.com",
+  projectId: "master-scheduler-cbe64",
+  storageBucket: "master-scheduler-cbe64.appspot.com",
+  messagingSenderId: "622291728396",
+  appId: "1:622291728396:web:8fb7b0a961b9e01e671629",
+  measurementId: "G-SEGL7BQCX6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Initialize Firestore
+const db = getFirestore(app);
+
+// Export Firestore
+export { db };
